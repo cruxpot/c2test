1f2fc719512bbc0bea016fb738a4a4e0c55957af
James Park <jpark@DEF-LOGIX.local>
2019-11-18 15:44:20 -0600

    test dollar sign bracket removal

--- modules/aws/create-vpc/main.tf
+++ modules/aws/create-vpc/main.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "default" {
   //count = "${var.varcount}"
   //provider = "aws.${element(var.regions, count.index)}"
 
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id = "aws_vpc.default.id"
   cidr_block = "10.0.0.0/24"
 }
 
